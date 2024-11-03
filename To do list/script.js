c78d04da0c1e96aba66fb1f5367e6b1b83507b99
Yuvraj singh <yuvrajs2122005@gmail.com>
2024-11-04 01:42:13 +0530

    changes in script

--- To do list/script.js
+++ To do list/script.js
@@ -1,47 +1,62 @@
-const input=document.querySelector(".inpbox input");
-const addbutton=document.querySelector(".inpbox span button");
-const taskbox=document.querySelector('.task-box');
-const task=document.querySelectorAll('.task');
-addbutton.addEventListener('click',(e)=>{
-    if(input.value===""){
+const input = document.querySelector(".inpbox input");
+const addbutton = document.querySelector(".inpbox span button");
+const taskbox = document.querySelector('.task-box');
+
+addbutton.addEventListener('click', () => {
+    if (input.value === "") {
         alert("Task can't be empty");
+    } else {
+        createTask(input.value);
+        input.value = ""; 
+        saveContent();
     }
-    else{
-        let isDefault=true;
-        let div=document.createElement('div');
-        div.classList='task';
-        div.innerHTML=input.value+`<span><button>Remove</button></span></div>`;
-        taskbox.appendChild(div)
-        input.value="";
-        const rembutton = div.querySelector("span button");
-            rembutton.addEventListener('click', () => {
-                taskbox.removeChild(div); 
-            });
-            div.addEventListener('click', () => {
-                div.classList.toggle('strike-through');
-                if(isDefault){
-                    div.style.setProperty("--task-icon", "url('/To do list/images/icons8-tick-in-circle-50.png')");
-                    div.style.opacity='.5'
-                    isDefault=false;
-                    saveContent();
-                }
-                else{
-                    div.style.opacity='1'
-                    div.style.setProperty("--task-icon", "url('/To do list/images/icons8-circle-50.png')");
-                    isDefault=true;   
-                    saveContent();
-                }
-            });
+});
+
+function createTask(content) {
+    const div = document.createElement('div');
+    div.classList.add('task');
+    div.innerHTML = `${content}<span><button>Remove</button></span>`;
+
+    taskbox.appendChild(div);
+
+    const rembutton = div.querySelector("span button");
+    rembutton.addEventListener('click', () => {
+        taskbox.removeChild(div);
+        saveContent(); 
+    });
+
+    div.addEventListener('click', () => {
+        div.classList.toggle('strike-through');
+        const currentIcon = div.style.getPropertyValue("--task-icon");
+        if (currentIcon.includes('icons8-tick')) {
+            div.style.setProperty("--task-icon", "url('/To do list/images/icons8-circle-50.png')");
+            div.style.opacity = '1';
+            saveContent();
+        } else {
+            div.style.setProperty("--task-icon", "url('/To do list/images/icons8-tick-in-circle-50.png')");
+            div.style.opacity = '0.5';
+            saveContent();
         }
         saveContent();
-    })
-    
-function saveContent(){
-    localStorage.setItem('data',taskbox.innerHTML);
+    });
 }
 
-function showContent(){
-    taskbox.innerHTML=localStorage.getItem('data');
+function saveContent() {
+    const tasks = Array.from(document.querySelectorAll('.task')).map(task => {
+        return {
+            content: task.childNodes[0].nodeValue.trim(),
+            isDefault: task.style.getPropertyValue("--task-icon").includes('circle'),
+            isStrikethrough: task.classList.contains('strike-through')
+        };
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function showContent() {
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    tasks.forEach(taskData => {
+        createTask(taskData.content, taskData.isDefault, taskData.isStrikethrough);
+    });
 }
 
 showContent();
